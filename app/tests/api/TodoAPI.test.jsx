665d16a504b36b441933885ed18e14ca54d5b16a
Joey Bryan <josephrbryan@gmail.com>
2017-01-24 21:50:31 -0800

    Added searchText Filter

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -56,17 +56,17 @@ describe('TodoAPI', () => {
     var todos = [
       {
         id: 1,
-        text: 'Test 1',
-        completed: false
-      },
-      {
-        id: 2,
-        text: 'Test 2',
+        text: 'An item',
         completed: true
       },
       {
+        id: 2,
+        text: 'Some other item',
+        completed: false
+      },
+      {
         id: 3,
-        text: 'Test 3',
+        text: 'Nothing to see here',
         completed: false
       }
     ];
@@ -79,6 +79,21 @@ describe('TodoAPI', () => {
     it('should only return todos with showCompleted of false', () => {
       var filteredTodos = TodoAPI.filterTodos(todos, false, '');
       expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
     });
   });
 });
