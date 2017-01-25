75c82c5ae44ca55e0532880cc53f690bafc60a86
Joey Bryan <josephrbryan@gmail.com>
2017-01-24 20:22:42 -0800

    Add Todo tests

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -2,11 +2,16 @@ var React = require('react');
 
 var Todo = React.createClass({
   render: function () {
-    var {id, text} = this.props;
+    var {id, text, completed} = this.props;
 
     return (
-      <div>
-        {id}. {text}
+      <div onClick={() => {
+          this.props.onToggle(id);
+        }}>
+        <input id={id} type="checkbox" checked={completed} readOnly />
+        <label htmlFor={id}>
+          {text}
+        </label>
       </div>
     )
   }
